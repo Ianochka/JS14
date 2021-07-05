45e68b5bf208ba81c06cc01f23f89350eb8fa303
Ianochka <iana.a.barabash@gmail.com>
2021-07-05 23:06:56 +0200

    Изменённый калькулятор

---
+++ calculator.js
@@ -0,0 +1,32 @@
+
+function sum() {
+    let number1, number2, result;
+    number1 = document.getElementById('number1').value;
+    number2 = document.getElementById('number2').value;
+    result = Number(number1) + Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
+
+function subtraction() {
+    let number1, number2, result;
+    number1 = document.getElementById('number1').value;
+    number2 = document.getElementById('number2').value;
+    result = Number(number1) - Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
+
+function division() {
+    let number1, number2, result;
+    number1 = document.getElementById('number1').value;
+    number2 = document.getElementById('number2').value;
+    result = Number(number1) / Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
+
+function multiplication() {
+    let number1, number2, result;
+    number1 = document.getElementById('number1').value;
+    number2 = document.getElementById('number2').value;
+    result = Number(number1) * Number(number2);
+    document.getElementById('result').innerHTML = result;
+}
